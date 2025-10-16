9b7ce4bf1e4b01ad9636456398efdf4c363b7ee4
Dogancan <dogancanturgut0@icloud.com>
2025-10-16 23:32:48 +0300

    Initial Commit

---
+++ learnswiftlesson2/learnswiftlesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  learnswiftlesson2App.swift
+//  learnswiftlesson2
+//
+//  Created by Dogancan turgut on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct learnswiftlesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
